26740195ff48e90345b63355b66218f5f3a2fad3
Kristian Carr <127743278+kcarr-bjss@users.noreply.github.com>
2023-10-31 15:05:23 +0000

    feature(cb2-9608): Added stubbed endpoints for IVA defects (#141)
    * feat(cb2-9608): added iva defects manual get endpoint
    
    * feat(cb2-9608): updated the types definition package to latest
    
    * feat(cb2-9608): added the getIvaDefects function and associated endpoint

---
+++ src/utils/httpHeaders.ts
@@ -0,0 +1,14 @@
+import { APIGatewayProxyResult } from "aws-lambda";
+
+export const addHttpHeaders = (
+  httpResponse: APIGatewayProxyResult,
+): APIGatewayProxyResult => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers":
+      "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+    "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
+  };
+
+  return { ...httpResponse, headers };
+};
