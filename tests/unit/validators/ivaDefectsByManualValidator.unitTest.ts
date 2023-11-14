6c56bb74d6e95a2161fe7828804250b17eb61ca6
kristian carr <kristian.carr@bjss.com>
2023-11-14 16:59:58 +0000

    feat(cb2-9608): more automatic linter changes

--- tests/unit/validators/ivaDefectsByManualValidator.unitTest.ts
+++ tests/unit/validators/ivaDefectsByManualValidator.unitTest.ts
@@ -1,48 +1,55 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
-import { validateIvaDefectManualQuery } from '../../../src/validators/iva/ivaDefectsByManualValidator';
-import { mockToken } from '../../util/mockToken';
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { validateIvaDefectManualQuery } from "../../../src/validators/iva/ivaDefectsByManualValidator";
+import { mockToken } from "../../util/mockToken";
 
-describe('test the iva defect manual query error validator', () => {
-  it('should return Missing authorization header', () => {
+describe("test the iva defect manual query error validator", () => {
+  it("should return Missing authorization header", () => {
     const event = {
-      pathParameters: { id: 'M1' },
+      pathParameters: { id: "M1" },
       headers: {},
     };
-    const res = validateIvaDefectManualQuery(event as unknown as APIGatewayProxyEvent);
+    const res = validateIvaDefectManualQuery(
+      event as unknown as APIGatewayProxyEvent,
+    );
     expect(res).toEqual({
       statusCode: 400,
       body: "Missing authorization header",
     });
   });
 
-  it('should return an error when missing a manual ID', () => {
+  it("should return an error when missing a manual ID", () => {
     const event = {
-      pathParameters: {},      
+      pathParameters: {},
       headers: {
         Authorization: mockToken,
       },
     };
-    const res = validateIvaDefectManualQuery(event as unknown as APIGatewayProxyEvent);
+    const res = validateIvaDefectManualQuery(
+      event as unknown as APIGatewayProxyEvent,
+    );
 
     expect(res).toEqual({
-      "statusCode": 400,
-      "body": "{\"errors\":[\"Missing id\"]}",
-      "headers": {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
-          "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS"
-      }
+      statusCode: 400,
+      body: '{"errors":["Missing id"]}',
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token",
+        "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS",
+      },
     });
   });
 
-  it('should return undefined when no errors', () => {
+  it("should return undefined when no errors", () => {
     const event = {
-        pathParameters: { id: 'M1' },
-        headers: {
+      pathParameters: { id: "M1" },
+      headers: {
         Authorization: mockToken,
       },
     };
-    const res = validateIvaDefectManualQuery(event as unknown as APIGatewayProxyEvent);
+    const res = validateIvaDefectManualQuery(
+      event as unknown as APIGatewayProxyEvent,
+    );
     expect(res).toBeUndefined();
   });
 });
--- tests/unit/validators/ivaDefectsGetValidator.unitTest.ts
+++ tests/unit/validators/ivaDefectsGetValidator.unitTest.ts
@@ -1,26 +1,30 @@
-import { APIGatewayProxyEvent } from 'aws-lambda';
-import { validateIvaDefectGetQuery } from '../../../src/validators/iva/ivaDefectsGetValidator';
-import { mockToken } from '../../util/mockToken';
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { validateIvaDefectGetQuery } from "../../../src/validators/iva/ivaDefectsGetValidator";
+import { mockToken } from "../../util/mockToken";
 
-describe('test the iva defect by get query error validator', () => {
-  it('should return Missing authorization header', () => {
+describe("test the iva defect by get query error validator", () => {
+  it("should return Missing authorization header", () => {
     const event = {
       headers: {},
     };
-    const res = validateIvaDefectGetQuery(event as unknown as APIGatewayProxyEvent);
+    const res = validateIvaDefectGetQuery(
+      event as unknown as APIGatewayProxyEvent,
+    );
     expect(res).toEqual({
       statusCode: 400,
       body: "Missing authorization header",
     });
   });
 
-  it('should return undefined when no errors', () => {
+  it("should return undefined when no errors", () => {
     const event = {
-        headers: {
+      headers: {
         Authorization: mockToken,
       },
     };
-    const res = validateIvaDefectGetQuery(event as unknown as APIGatewayProxyEvent);
+    const res = validateIvaDefectGetQuery(
+      event as unknown as APIGatewayProxyEvent,
+    );
     expect(res).toBeUndefined();
   });
 });
