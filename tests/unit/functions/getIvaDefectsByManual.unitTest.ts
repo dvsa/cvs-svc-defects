b83fb4d096dac75d51f8725fb3823a27abf154a1
kristian carr <kristian.carr@bjss.com>
2023-11-15 15:40:45 +0000

    feat(cb2-9608): linter

--- tests/unit/functions/getIvaDefectsByManual.unitTest.ts
+++ tests/unit/functions/getIvaDefectsByManual.unitTest.ts
@@ -1,7 +1,7 @@
 const mockValidateIvaDefectManualQuery = jest.fn();
 
-jest.mock('../../../src/validators/iva/ivaDefectsByManualValidator', () => ({
-    validateIvaDefectManualQuery: mockValidateIvaDefectManualQuery
+jest.mock("../../../src/validators/iva/ivaDefectsByManualValidator", () => ({
+  validateIvaDefectManualQuery: mockValidateIvaDefectManualQuery,
 }));
 
 import { getIvaDefectsByManual } from "../../../src/functions/getIvaDefectsByManual";
@@ -12,46 +12,58 @@ import { HTTPError } from "../../../src/models/HTTPError";
 
 describe("getDefects Function", () => {
   const ctx = mockContext as Context;
-  
+
   context("on successful retrieval of results", () => {
     it("returns 200 with data", async () => {
-        mockValidateIvaDefectManualQuery.mockReturnValueOnce(undefined);
-        jest.spyOn(IvaDefectsService.prototype, "getIvaDefectsByManualId")
-            .mockReturnValue(Promise.resolve([]));
-        const event = {
-            pathParameters: {
-                id: "M1"
-            }
-        };
-        
-        const res = await getIvaDefectsByManual(event, ctx, () => { return; });
-        expect(res).toEqual(new HTTPResponse(200, []));
+      mockValidateIvaDefectManualQuery.mockReturnValueOnce(undefined);
+      jest
+        .spyOn(IvaDefectsService.prototype, "getIvaDefectsByManualId")
+        .mockReturnValue(Promise.resolve([]));
+      const event = {
+        pathParameters: {
+          id: "M1",
+        },
+      };
+
+      const res = await getIvaDefectsByManual(event, ctx, () => {
+        return;
+      });
+      expect(res).toEqual(new HTTPResponse(200, []));
     });
   });
 
-  context("on validation failure returns expected client error status code", () => {
-    it("returns 400 with errors", async () => {
+  context(
+    "on validation failure returns expected client error status code",
+    () => {
+      it("returns 400 with errors", async () => {
         mockValidateIvaDefectManualQuery.mockReturnValueOnce({
-            statusCode: 400,
-            body: JSON.stringify({ errors: ["Fake Error"] }),
+          statusCode: 400,
+          body: JSON.stringify({ errors: ["Fake Error"] }),
         });
-        jest.spyOn(IvaDefectsService.prototype, "getIvaDefectsByManualId")
-            .mockReturnValue(Promise.resolve([]));
+        jest
+          .spyOn(IvaDefectsService.prototype, "getIvaDefectsByManualId")
+          .mockReturnValue(Promise.resolve([]));
         const event = {
-            pathParameters: {
-                id: "M1"
-            }
+          pathParameters: {
+            id: "M1",
+          },
         };
 
-        const res = await getIvaDefectsByManual(event, ctx, () => { return; });
+        const res = await getIvaDefectsByManual(event, ctx, () => {
+          return;
+        });
         expect(res.statusCode).toEqual(400);
-    });
-  });
+      });
+    },
+  );
   context("on an internal server error returns expected error code", () => {
     it("returns 500 with error", async () => {
-        mockValidateIvaDefectManualQuery.mockReturnValueOnce(undefined);
-        jest.spyOn(IvaDefectsService.prototype, "getIvaDefectsByManualId")
-            .mockReturnValue(Promise.reject(new HTTPError(500, "Internal Server Error")));
+      mockValidateIvaDefectManualQuery.mockReturnValueOnce(undefined);
+      jest
+        .spyOn(IvaDefectsService.prototype, "getIvaDefectsByManualId")
+        .mockReturnValue(
+          Promise.reject(new HTTPError(500, "Internal Server Error")),
+        );
       const res = await getIvaDefectsByManual(null, ctx, () => {
         return;
       });
