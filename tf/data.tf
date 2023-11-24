4e4bdb09b44793783ffeac8d9296e1aef6e0bd2e
Matt Waldron <matt.waldron@bjss.com>
2023-11-24 11:48:16 +0000

    chore(cb2-9827): try SM with existing assumed role

--- tf/data.tf
+++ tf/data.tf
@@ -1,10 +1,8 @@
 data "aws_secretsmanager_secret" "acct_ids" {
-  provider = aws.mgmt
   name     = "terraform/account-ids"
 }
 
 data "aws_secretsmanager_secret_version" "acct_ids" {
-  provider  = aws.mgmt
   secret_id = data.aws_secretsmanager_secret.acct_ids.id
 }
 
