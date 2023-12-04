32febd73e902817d9d322b25e9139b722b16719d
Matt Waldron <matt.waldron@bjss.com>
2023-12-04 11:36:36 +0000

    chore(cb2-9827): remove comments

--- tf/variables.tf
+++ tf/variables.tf
@@ -1,15 +1,3 @@
-# variable "component" {
-#   description = "Service component e.g. dft"
-#   type        = string
-#   default     = "dft"
-# }
-
-# variable "project" {
-#   type        = string
-#   description = "The name of the tfscaffold project"
-#   default     = "cvs"
-# 
-
 variable "bucket_name" {
     type = string
     description = "The bucket where the service artefact is stored"
