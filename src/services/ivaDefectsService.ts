e7351dcbab57571734d7b3d43babce52daf4c85a
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2023-12-21 14:39:37 +0000

    refactor

--- src/services/ivaDefectsService.ts
+++ src/services/ivaDefectsService.ts
@@ -1,129 +1,142 @@
 import {EUVehicleCategory} from "@dvsa/cvs-type-definitions/types/v3/tech-record/enums/euVehicleCategory.enum";
 import {IvaDatabaseService} from "./ivaDatabaseService";
 import {HTTPError} from "../models/HTTPError";
-import {SectionIVA, newDefectGETIVA, IIVADefect} from "../models/IVADefect";
+import {IIVADefect, newDefectGETIVA, SectionIVA} from "../models/IVADefect";
 import {DefectGETIVA, InspectionType} from "@dvsa/cvs-type-definitions/types/iva/defects/get";
-import {IRequiredStandard} from "../models/RequiredStandard";
 
 
 export class IvaDefectsService {
-  public readonly ivaDatabaseService: IvaDatabaseService;
+    public readonly ivaDatabaseService: IvaDatabaseService;
 
-  /**
-   * Constructor for the IvaDefectsService class
-   * @param ivaDatabaseService the IVA database service
-   */
-  constructor(ivaDatabaseService: IvaDatabaseService) {
-    this.ivaDatabaseService = ivaDatabaseService;
-  }
-
-  /**
-   * Retrieves IVA Defects based on the optionally provided vehicleType, euVehicleCategory and inspectionType and formats the response
-   * @param vehicleType the type of Vehicle e.g psv, lgv
-   * @param euVehicleCategory the EU Vehicle Category, synonymous with Manual ID
-   * @param basicInspection the Inspection Type e.g basic, normal
-   * @returns Array of IVA Defects
-   */
-  public async getIvaDefects(
-    euVehicleCategory: EUVehicleCategory | null,
-    basicInspection: boolean | false,
-  ): Promise<DefectGETIVA[]> {
-    try {
-      const results = await this.ivaDatabaseService.getDefectsByCriteria(
-        euVehicleCategory,
-        basicInspection,
-      );
-      const formattedResults: DefectGETIVA[] = [];
-      return formattedResults;
-    } catch (error: any) {
-      if (!(error instanceof HTTPError)) {
-        console.error(error);
-        error.statusCode = 500;
-        error.body = "Internal Server Error";
-      }
-      throw new HTTPError(error.statusCode, error.body);
+    /**
+     * Constructor for the IvaDefectsService class
+     * @param ivaDatabaseService the IVA database service
+     */
+    constructor(ivaDatabaseService: IvaDatabaseService) {
+        this.ivaDatabaseService = ivaDatabaseService;
     }
-  }
 
-  /**
-   * Retrieves IVA Defects based on the provided manualId and formats the response
-   * @param euVehicleCategory the manual ID, e.g M1, N1, MSVA
-   * @returns Array of IVA Defects
-   */
-  public async getIvaDefectsByEUVehicleCategory(
-    euVehicleCategory: string,
-  ): Promise<newDefectGETIVA> {
-    try {
-      const results =
-        (await this.ivaDatabaseService.getDefectsByEUVehicleCategory(
-            euVehicleCategory
-        )) as IIVADefect[];
+    /**
+     * Retrieves IVA Defects based on the optionally provided euVehicleCategory, and inspectionType and formats the response
+     * @param euVehicleCategory the EU Vehicle Category
+     * @param basicInspection the Inspection Type
+     * @returns Array of IVA Defects
+     */
+    public async getIvaDefects(
+        euVehicleCategory: EUVehicleCategory | null,
+        basicInspection: boolean | false
+    ): Promise<DefectGETIVA[]> {
+        try {
+            const results = await this.ivaDatabaseService.getDefectsByCriteria(
+                euVehicleCategory,
+                basicInspection
+            );
 
-      return this.formatIvaDefects(results, euVehicleCategory);
-    } catch (error: any) {
-      if (!(error instanceof HTTPError)) {
-        console.error(error);
-        error.statusCode = 500;
-        error.body = "Internal Server Error";
-      }
-      throw new HTTPError(error.statusCode, error.body);
+            return [];
+        } catch (error) {
+            this.handleUnexpectedError(error);
+            throw new HTTPError(500, "Internal Server Error");
+        }
     }
-  }
 
-  public getEnumKeyByEnumValue<T extends { [index: string]: string }>(
-    myEnum: T,
-    enumValue: string,
-  ): keyof T {
-    const keys = Object.keys(myEnum).filter((x) => myEnum[x] === enumValue);
-    return keys[0];
-  }
+    /**
+     * Retrieves IVA Defects based on the provided manualId and formats the response
+     * @param euVehicleCategory euVehicleCategory id
+     * @returns Array of IVA Defects
+     */
+    public async getIvaDefectsByEUVehicleCategory(
+        euVehicleCategory: string
+    ): Promise<newDefectGETIVA> {
+        try {
+            const results = (await this.ivaDatabaseService.getDefectsByEUVehicleCategory(
+                euVehicleCategory
+            )) as IIVADefect[];
 
+            return this.formatIvaDefects(results, euVehicleCategory);
+        } catch (error) {
+            this.handleUnexpectedError(error);
+            throw new HTTPError(500, "Internal Server Error");
+        }
+    }
 
-  public formatInspectionTypes(sections : IIVADefect[]) : SectionIVA[] {
-      const formattedSections : SectionIVA[] = sections.map((x ) => {
-           const rsArr = x.requiredStandards.map(dx =>{
-              return {
-                  rsNumber: parseInt(dx.rsNumber, 10),
-                  requiredStandard: dx.requiredStandard,
-                  refCalculation: dx.refCalculation,
-                  additionalInfo: dx.additionalInfo,
-                  inspectionTypes: dx.normalInspection && dx.basicInspection ? ["basic","normal"] as InspectionType[]:
-                      dx.basicInspection ? ["basic"] as InspectionType[]: ["normal"] as InspectionType[]
-              }
-          });
-          return {
-              sectionNumber: x.sectionNumber,
-              sectionDescription: x.sectionDescription,
-              requiredStandards: rsArr
-          } as SectionIVA
-      });
-      return formattedSections;
-  }
-public formatIvaDefects(results: IIVADefect[], euVehicleCategory : string): newDefectGETIVA {
-      const vehiclecat = this.getEnumKeyByEnumValue(EUVehicleCategory, euVehicleCategory);
-      const basicSections: IIVADefect[] = results.filter(x=> {
-          x.requiredStandards = x.requiredStandards.filter(dx => {
-              if(dx.basicInspection){
-                  return dx
-              }
-          });
-          if(x.requiredStandards.length > 0) {
-              return x;
-          }
-      });
-      const normalSections: IIVADefect[] = results.filter(y => {
-          y.requiredStandards = y.requiredStandards.filter(dy => {
-              if(dy.normalInspection || (!dy.basicInspection && !dy.normalInspection)){
-                  return dy;
-              }
-          });
-          if(y.requiredStandards.length > 0) return y;
-      });
+    /**
+     * Helper function to handle unexpected errors
+     * @param error the error object
+     */
+    private handleUnexpectedError(error: any): void {
+        if (!(error instanceof HTTPError)) {
+            console.error("Unexpected error:", error);
+        }
+    }
 
-      return {
-          euVehicleCategory: vehiclecat,
-          basic: this.formatInspectionTypes(basicSections),
-          normal: this.formatInspectionTypes(normalSections)
-      } as newDefectGETIVA
-  }
+    /**
+     * Get enum key by enum value
+     * @param myEnum the enum object
+     * @param enumValue the enum value
+     * @returns the enum key or undefined
+     */
+    public getEnumKeyByEnumValue<T extends Record<string, string>>(
+        myEnum: T,
+        enumValue: string
+    ): keyof T | undefined {
+        return Object.keys(myEnum).find((key) => myEnum[key] === enumValue);
+    }
+
+    /**
+     * Transforms an array of standards by adding an inspectionTypes property based on normalInspection and basicInspection
+     * @param {any[]} standards - The array of standards to transform.
+     * @returns {any[]} - The transformed array of standards.
+     */
+    private transformStandards(standards: any[]): any[] {
+        return standards
+            .map((x) => ({
+                ...x,
+                inspectionTypes: (x.normalInspection && x.basicInspection)
+                    ? ["basic", "normal"] as InspectionType[]
+                    : x.basicInspection
+                        ? ["basic"] as InspectionType[]
+                        : ["normal"] as InspectionType[],
+            }))
+            .filter((std) => std.inspectionTypes.length > 0);
+    }
+
+    /**
+     * Formats IVA Defects inspection types based on the provided section
+     * @param {IIVADefect[]} sections - Array of IVA Defect sections
+     * @returns {SectionIVA[]} - Formatted array of SectionIVA.
+     */
+    public formatInspectionTypes(sections: IIVADefect[]): SectionIVA[] {
+        return sections
+            .map((x) => ({
+                sectionNumber: x.sectionNumber,
+                sectionDescription: x.sectionDescription,
+                requiredStandards: this.transformStandards(x.requiredStandards),
+            }))
+            .filter((section) => section.requiredStandards.length > 0) as SectionIVA[];
+    }
+
+    /**
+     * Formats IVA Defects based on the provided results and EU vehicle category
+     * @param {IIVADefect[]} results - Array of IVA Defect results
+     * @param {string} euVehicleCategory - EU vehicle category
+     * @returns {newDefectGETIVA} - Formatted IVA Defects object
+     */
+    public formatIvaDefects(results: IIVADefect[], euVehicleCategory: string): newDefectGETIVA {
+        const euVehicleCategoryFromEnum = this.getEnumKeyByEnumValue(EUVehicleCategory, euVehicleCategory) || '';
+
+        const filterSections = (filterFn: (x: any) => boolean) => results
+            .filter((x) => x.requiredStandards.some(filterFn))
+            .map(({ requiredStandards, ...x }) => ({
+                ...x,
+                requiredStandards: this.transformStandards(requiredStandards),
+            }));
+
+        return {
+            euVehicleCategory: euVehicleCategoryFromEnum,
+            basic: this.formatInspectionTypes(filterSections((x) => x.basicInspection)),
+            normal: this.formatInspectionTypes(
+                filterSections((x) => x.normalInspection || (!x.basicInspection && !x.normalInspection))
+            ),
+        };
+    }
 }
