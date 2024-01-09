1e2558efe93881e97d74fcfa1bd9e68024b9037a
kristian carr <kristian.carr@bjss.com>
2024-01-09 10:38:26 +0000

    feat(cb2-10283): added further iva defects service tests

--- tests/unit/services/ivaDefectsService.unitTest.ts
+++ tests/unit/services/ivaDefectsService.unitTest.ts
@@ -6,6 +6,7 @@ import { EUVehicleCategory } from "@dvsa/cvs-type-definitions/types/iva/defects/
 import { HTTPError } from "../../../src/models/HTTPError";
 import { IvaDefectsService } from "../../../src/services/ivaDefectsService";
 import IvaDefects from "../../resources/iva-defects.json";
+import { IIVATaxonomySection } from "../../../src/models/IVADefect";
 
 const mockGetDefectsByEUVehicleCategory = jest.fn();
 const mockGetDefectsByCriteria = jest.fn();
@@ -20,6 +21,164 @@ describe("IVA Defects Service", () => {
   beforeEach(() => {
     jest.resetAllMocks();
     jest.resetModules();
+  });
+
+  describe("formatIvaDefects", () => {
+    it("should return a correctly formatted section with basic required standards only", () => {
+      const taxonomySection: IIVATaxonomySection[] = [
+        {
+          euVehicleCategory: "m1",
+          sectionNumber: "01",
+          sectionDescription: "Noise",
+          requiredStandards: [
+            {
+              rsNumber: "1",
+              requiredStandard: "A test standard",
+              refCalculation: "1.1",
+              additionalInfo: true,
+              basicInspection: true,
+              normalInspection: false,
+            },
+          ],
+        },
+      ];
+
+      const result = target.formatIvaDefects(
+        taxonomySection,
+        EUVehicleCategory.M1,
+      );
+
+      expect(result?.euVehicleCategories?.at(0)).toBe("m1");
+      expect(result?.basic?.at(0)?.sectionDescription).toBe("Noise");
+      expect(result?.basic?.at(0)?.sectionNumber).toBe("01");
+      expect(result?.basic?.at(0)?.requiredStandards.length).toBe(1);
+      expect(result?.normal?.length).toBe(0);
+    });
+
+    it("should return a correctly formatted section with normal and basic required standards", () => {
+      const taxonomySection: IIVATaxonomySection[] = [
+        {
+          euVehicleCategory: "m1",
+          sectionNumber: "01",
+          sectionDescription: "Noise",
+          requiredStandards: [
+            {
+              rsNumber: "1",
+              requiredStandard: "A test standard",
+              refCalculation: "1.1",
+              additionalInfo: true,
+              basicInspection: true,
+              normalInspection: true,
+            },
+          ],
+        },
+      ];
+
+      const result = target.formatIvaDefects(
+        taxonomySection,
+        EUVehicleCategory.M1,
+      );
+
+      expect(result?.euVehicleCategories?.at(0)).toBe("m1");
+      expect(result?.normal?.at(0)?.sectionDescription).toBe("Noise");
+      expect(result?.normal?.at(0)?.sectionNumber).toBe("01");
+      expect(result?.normal?.at(0)?.requiredStandards.length).toBe(1);
+
+      expect(result?.basic?.at(0)?.sectionDescription).toBe("Noise");
+      expect(result?.basic?.at(0)?.sectionNumber).toBe("01");
+      expect(result?.basic?.at(0)?.requiredStandards.length).toBe(1);
+      expect(result?.basic?.length).toBe(1);
+    });
+
+    it("should return multiple correctly formatted sections with normal and basic required standards", () => {
+      const taxonomySection: IIVATaxonomySection[] = [
+        {
+          euVehicleCategory: "m1",
+          sectionNumber: "01",
+          sectionDescription: "Noise",
+          requiredStandards: [
+            {
+              rsNumber: "1",
+              requiredStandard: "A test standard",
+              refCalculation: "1.1",
+              additionalInfo: true,
+              basicInspection: true,
+              normalInspection: true,
+            },
+            {
+              rsNumber: "1",
+              requiredStandard: "Another test standard",
+              refCalculation: "1.1",
+              additionalInfo: true,
+              basicInspection: true,
+              normalInspection: true,
+            },
+          ],
+        },
+      ];
+
+      const result = target.formatIvaDefects(
+        taxonomySection,
+        EUVehicleCategory.M1,
+      );
+
+      expect(result?.euVehicleCategories?.at(0)).toBe("m1");
+      expect(result?.normal?.length).toBe(1);
+      expect(result?.normal?.at(0)?.sectionDescription).toBe("Noise");
+      expect(result?.normal?.at(0)?.sectionNumber).toBe("01");
+      expect(result?.normal?.at(0)?.requiredStandards.length).toBe(2);
+
+      expect(result?.basic?.length).toBe(1);
+      expect(result?.basic?.at(0)?.sectionDescription).toBe("Noise");
+      expect(result?.basic?.at(0)?.sectionNumber).toBe("01");
+      expect(result?.basic?.at(0)?.requiredStandards.length).toBe(2);
+      expect(result?.basic?.length).toBe(1);
+    });
+
+    it("should return an empty array when provided empty taxonomy sections", () => {
+      const taxonomySection: IIVATaxonomySection[] = [];
+      const result = target.formatIvaDefects(
+        taxonomySection,
+        EUVehicleCategory.M1,
+      );
+
+      expect(result?.euVehicleCategories?.at(0)).toBe("m1");
+      expect(result?.normal?.length).toBe(0);
+      expect(result?.basic?.length).toBe(0);
+    });
+
+    it("should return correctly formatted required standards", () => {
+      const taxonomySection: IIVATaxonomySection[] = [
+        {
+          euVehicleCategory: "m1",
+          sectionNumber: "01",
+          sectionDescription: "Noise",
+          requiredStandards: [
+            {
+              rsNumber: "1",
+              requiredStandard: "A test standard",
+              refCalculation: "1.1",
+              additionalInfo: true,
+              basicInspection: true,
+              normalInspection: false,
+            },
+          ],
+        },
+      ];
+
+      const result = target.formatIvaDefects(
+        taxonomySection,
+        EUVehicleCategory.M1,
+      );
+      const requiredStandards = result?.basic?.at(0)?.requiredStandards;
+      const actual = requiredStandards?.at(0);
+
+      expect(requiredStandards?.length).toBe(1);
+      expect(actual?.rsNumber).toBe(1);
+      expect(actual?.requiredStandard).toBe("A test standard");
+      expect(actual?.refCalculation).toBe("1.1");
+      expect(actual?.additionalInfo).toBe(true);
+    });
   });
 
   describe("getIvaDefectsByEUVehicleCategory", () => {
@@ -51,18 +210,6 @@ describe("IVA Defects Service", () => {
       expect(mockGetDefectsByEUVehicleCategory).toHaveBeenCalledTimes(1);
       expect(result?.euVehicleCategories?.length).toBe(1);
       expect(result?.euVehicleCategories.at(0)).toBe(EUVehicleCategory.M1);
-    });
-
-    it("should return a correctly formatted section upon successful result", async () => {
-      mockGetDefectsByEUVehicleCategory.mockResolvedValueOnce(IvaDefects);
-
-      const result = await target.getIvaDefectsByEUVehicleCategory("M1");
-      const resultSection = result?.normal?.at(0);
-
-      expect(mockGetDefectsByEUVehicleCategory).toHaveBeenCalledTimes(1);
-      expect(resultSection?.sectionNumber).toBe("01");
-      expect(resultSection?.sectionDescription).toBe("Noise");
-      expect(resultSection?.requiredStandards?.length).toBe(6);
     });
 
     it("should return an empty basic array upon successfully finding no search results", async () => {
