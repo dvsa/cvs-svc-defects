8a6b0b45f213765664509a383cdabd1ddc6bc089
JunYanBJSS <jun.yan@bjss.com>
2024-03-19 13:54:34 +0000

    feat(cb2-11225): lint

--- scripts/tearDown.ts
+++ scripts/tearDown.ts
@@ -1,7 +1,6 @@
 import { killTestSetup } from "./destroyServices";
 
 module.exports = async () => {
-
   // Serverless runs containers and pid's are not managed the same way in the CI/CD pipeline
   // We are not using the pid of our instance to kill the task
   // instead created sh commands to manually kill the webserver and dynamoDB instances which work equally in the pipeline
@@ -9,7 +8,6 @@ module.exports = async () => {
   try {
     await killTestSetup();
   } catch (e) {
-
     throw e;
   }
 };
