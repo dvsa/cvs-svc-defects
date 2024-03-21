50a1a00b5f879f2cabf5a853ad3905f15dacc39b
JunYanBJSS <144220886+JunYanBJSS@users.noreply.github.com>
2024-03-21 09:37:22 +0000

    Feat/cb2-11225 Update cvs-svc-defects backend repo to use serverless-dynamodb (#193)

--- scripts/tearDown.ts
+++ scripts/tearDown.ts
@@ -2,9 +2,9 @@ import { killTestSetup } from "./destroyServices";
 
 module.exports = async () => {
   console.log(`
-      Integration tests ✅
-      ...Tearing down tests...
-    `);
+  Integration tests ✅
+  ...Tearing down tests...
+`);
   // Serverless runs containers and pid's are not managed the same way in the CI/CD pipeline
   // We are not using the pid of our instance to kill the task
   // instead created sh commands to manually kill the webserver and dynamoDB instances which work equally in the pipeline
