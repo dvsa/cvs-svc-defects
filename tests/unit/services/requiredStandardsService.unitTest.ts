a1f0291d6e443cfdd6cc91b3cf4f4e49282899d4
Daniel Searle <daniel.searle1@dvsa.gov.uk>
2024-08-27 15:18:41 +0100

    feat(cb2-13700): package lock fix

--- tests/unit/services/requiredStandardsService.unitTest.ts
+++ tests/unit/services/requiredStandardsService.unitTest.ts
@@ -272,8 +272,7 @@ describe("required standards  Service", () => {
         RequiredStandards,
       );
 
-      const result =
-        await target.getRequiredStandardsByEUVehicleCategory("test");
+      const result = await target.getRequiredStandardsByEUVehicleCategory("test");
 
       expect(mockGetDefectsByEUVehicleCategory).toHaveBeenCalledTimes(1);
       expect(result?.normal?.length).toBe(635);
@@ -284,8 +283,7 @@ describe("required standards  Service", () => {
         RequiredStandards,
       );
 
-      const result =
-        await target.getRequiredStandardsByEUVehicleCategory("test");
+      const result = await target.getRequiredStandardsByEUVehicleCategory("test");
 
       expect(mockGetDefectsByEUVehicleCategory).toHaveBeenCalledTimes(1);
       expect(result?.basic?.length).toBe(96);
